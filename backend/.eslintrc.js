bd52ff1c2fc3ba94f764aba8a77b56c55462fe53
Stefano Saffran <stefanosaffran@hotmail.com>
2020-03-17 12:14:26 +0100

    feat: add config/lint files

---
+++ backend/.eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+    jest: true,
+  },
+  extends: [
+    'airbnb-base', 'prettier'
+  ],
+  plugins: [ 'prettier' ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    "prettier/prettier": "error",
+/*     "quotes": [
+      "error",
+      "single"
+    ], */
+		"newline-per-chained-call": ["error", { "ignoreChainWithDepth": 2 }],
+    "semi": 0,
+    "eqeqeq": "error",
+    "no-trailing-spaces": "error",
+    "object-curly-spacing": [
+      "error", "always"
+    ],
+    "arrow-spacing": [
+      "error", { "before": true, "after": true }
+    ],
+    "no-console": 0,
+    "react/prop-types": 0,
+    "class-methods-use-this": "off",
+    "no-param-reassign": "off",
+    "camelcase": "off",
+    "no-unused-vars": ["error", { "argsIgnorePattern": "next" }],
+
+  },
+};
