217da6787b3680a1ed384a0c97120b32fe5599ec
Stefano Saffran <stefanosaffran@hotmail.com>
2020-03-17 12:21:39 +0100

    feat: add application config files

---
+++ backend/src/config/database.js
@@ -0,0 +1,16 @@
+require('../bootstrap');
+
+module.exports = {
+  dialect: process.env.DB_DIALECT || 'postgres',
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  storage: './__tests__/database.sqlite',
+  logging: false,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
