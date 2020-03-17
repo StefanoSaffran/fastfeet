17d76fdeaea8fd10559cdeba53fcfee34f126135
Stefano Saffran <stefanosaffran@hotmail.com>
2020-03-17 12:19:56 +0100

    feat: add routes file

---
+++ backend/src/routes.js
@@ -0,0 +1,57 @@
+import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
+
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+import RecipientController from './app/controllers/RecipientController';
+import FileController from './app/controllers/FileController';
+import DeliverymanController from './app/controllers/DeliverymanController';
+import DeliveryController from './app/controllers/DeliveryController';
+import StatusController from './app/controllers/StatusController';
+import DeliveryProblemsCtrller from './app/controllers/DeliveryProblemController';
+
+import authMiddleware from './app/middlewares/auth';
+
+const routes = new Router();
+const upload = multer(multerConfig);
+
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
+
+routes.get('/deliveryman/:id/deliveries', StatusController.index);
+routes.put('/deliveryman/:id/deliveries/:deliveryId', StatusController.update);
+
+routes.post('/delivery/:id/problems', DeliveryProblemsCtrller.store);
+routes.delete('/problem/:id/cancel-delivery', DeliveryProblemsCtrller.delete);
+
+/**
+ * Authentication middleware, all routes that
+ * need authenticated user comes after this
+ */
+routes.use(authMiddleware);
+
+routes.get('/delivery/problems', DeliveryProblemsCtrller.index);
+routes.get('/delivery/:id/problems', DeliveryProblemsCtrller.show);
+
+routes.post('/recipients', RecipientController.store);
+routes.get('/recipients', RecipientController.index);
+routes.get('/recipients/:id', RecipientController.show);
+routes.put('/recipients/:id', RecipientController.update);
+routes.delete('/recipients/:id', RecipientController.delete);
+
+routes.post('/files', upload.single('file'), FileController.store);
+
+routes.post('/deliveryman', DeliverymanController.store);
+routes.get('/deliveryman', DeliverymanController.index);
+routes.get('/deliveryman/:id', DeliverymanController.show);
+routes.put('/deliveryman/:id', DeliverymanController.update);
+routes.delete('/deliveryman/:id', DeliverymanController.delete);
+
+routes.post('/delivery', DeliveryController.store);
+routes.get('/delivery', DeliveryController.index);
+routes.get('/delivery/:id', DeliveryController.show);
+routes.put('/delivery/:id', DeliveryController.update);
+routes.delete('/delivery/:id', DeliveryController.delete);
+
+export default routes;
