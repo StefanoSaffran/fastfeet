b1bcc474e9f95d3938ae9893c854d8db4f3c414f
Stefano Saffran <stefanosaffran@hotmail.com>
2020-03-19 08:57:28 +0100

    feat: add app Routes

---
+++ mobile/src/Routes/DeliveryRoutes/index.js
@@ -0,0 +1,99 @@
+import React from 'react';
+import { TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { MaterialIcons as Icon } from '@expo/vector-icons';
+
+import Deliveries from '~/pages/Deliveries';
+import Details from '~/pages/Deliveries/Details';
+import NewProblem from '~/pages/Deliveries/NewProblem';
+import Problems from '~/pages/Deliveries/Problems';
+import Confirm from '~/pages/Deliveries/Confirm';
+
+const Stack = createStackNavigator();
+
+export default function DeliveryRoutes() {
+  const navigation = useNavigation();
+
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerTransparent: true,
+        headerTintColor: '#FFF',
+        headerLeftContainerStyle: {
+          marginLeft: 20,
+        },
+      }}
+    >
+      <Stack.Screen
+        name="Deliveries"
+        component={Deliveries}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="Details"
+        component={Details}
+        options={{
+          title: 'Detalhes da encomenda',
+          headerLeft: () => (
+            <TouchableOpacity
+              onPress={() => {
+                navigation.goBack();
+              }}
+            >
+              <Icon name="chevron-left" size={20} color="#FFF" />
+            </TouchableOpacity>
+          ),
+        }}
+      />
+      <Stack.Screen
+        name="NewProblem"
+        component={NewProblem}
+        options={{
+          title: 'Informar problema',
+          headerLeft: () => (
+            <TouchableOpacity
+              onPress={() => {
+                navigation.goBack();
+              }}
+            >
+              <Icon name="chevron-left" size={20} color="#FFF" />
+            </TouchableOpacity>
+          ),
+        }}
+      />
+      <Stack.Screen
+        name="Problems"
+        component={Problems}
+        options={{
+          title: 'Visualizar problemas',
+          headerLeft: () => (
+            <TouchableOpacity
+              onPress={() => {
+                navigation.goBack();
+              }}
+            >
+              <Icon name="chevron-left" size={20} color="#FFF" />
+            </TouchableOpacity>
+          ),
+        }}
+      />
+      <Stack.Screen
+        name="Confirm"
+        component={Confirm}
+        options={{
+          title: 'Confirmar agendamento',
+          headerLeft: () => (
+            <TouchableOpacity
+              onPress={() => {
+                navigation.goBack();
+              }}
+            >
+              <Icon name="chevron-left" size={20} color="#FFF" />
+            </TouchableOpacity>
+          ),
+        }}
+      />
+    </Stack.Navigator>
+  );
+}
