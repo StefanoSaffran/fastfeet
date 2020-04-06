431498a232a74cf504a283a97ff6569efcb9570b
Stefano Saffran <stefanosaffran@hotmail.com>
2020-04-06 16:44:16 +0200

    fix: bug when generated color has only 4-5 characters

--- frontend/src/components/Avatar/styles.js
+++ frontend/src/components/Avatar/styles.js
@@ -1,8 +1,9 @@
 import styled from 'styled-components';
+import { shade } from 'polished';
 
 export const Container = styled.div`
   margin-right: 5px;
-  color: ${({ color }) => color};
+  color: ${({ color }) => `${shade(0.09, `${color}`)}`};
   background: ${({ color }) => `${color}33`};
   width: 35px;
   height: 35px;
