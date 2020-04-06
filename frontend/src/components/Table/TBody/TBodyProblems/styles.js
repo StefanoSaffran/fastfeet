f82b597a45160f8baee138715f186e228c68dfad
Stefano Saffran <stefanosaffran@hotmail.com>
2020-04-06 11:25:33 +0200

    chore: remove lib/add css to add ellipsis for long problem description

--- frontend/src/components/Table/TBody/TBodyProblems/styles.js
+++ frontend/src/components/Table/TBody/TBodyProblems/styles.js
@@ -10,5 +10,23 @@ export const Container = styled.tbody`
     td:first-child {
       padding-left: 8px;
     }
+
+    td:nth-last-child(2) {
+      position: relative;
+
+      span {
+        position: absolute;
+        left: 0;
+        right: 0;
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
+      }
+    }
+
+    td:nth-last-child(2):before {
+      content: '&nbsp;';
+      visibility: hidden;
+    }
   }
 `;
